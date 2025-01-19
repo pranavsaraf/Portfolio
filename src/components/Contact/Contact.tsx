5d4ad4587857f9699bdd01c7d3d90b82a8003210
Pranav <pranavsaraf422@gmail.com>
2025-01-19 16:03:00 +0530

    Initial Commit

---
+++ src/components/Contact/Contact.tsx
@@ -0,0 +1,27 @@
+import { Container } from "./styles";
+import emailIcon from "../../assets/email-icon.svg";
+import phoneIcon from "../../assets/phone-icon.svg"
+import { Form } from "../Form/Form";
+
+
+export function Contact(){
+
+  return(
+    <Container id="contact">
+      <header>
+        <h2>Let's Connect | Contact</h2>
+      </header>
+      <div className="contacts">
+        <div>
+        <a href="mailto:pranavsaraf422@gmail.com"><img src={emailIcon} alt="Email" /></a> 
+          <a href="mailto:pranavsaraf422@gmail.com">pranavsaraf422@gmail.com</a>
+        </div>
+        <div>
+        <a href="tel:+919970706869"><img src={phoneIcon} alt="Phone No" /></a>
+          <a href="tel:+919970706869">(+91) 9970706869</a>
+        </div>  
+      </div>
+      <Form></Form>
+    </Container>
+  )
+}
