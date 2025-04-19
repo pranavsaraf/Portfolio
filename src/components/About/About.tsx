aa462af1a8035af825e4f234b1dbb4298319a151
Pranav <pranavsaraf422@gmail.com>
2025-04-19 12:39:42 +0530

    kafka added as skill

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -19,79 +19,79 @@ export function About() {
         <ScrollAnimation animateIn="fadeInLeft">
           <h2>About me</h2>
         </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInLeft" delay={0.1 * 1000}>
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.1 * 500}>
           <p>
           I'm Pranav, a Software Developer passionate about building scalable and efficient solutions. With hands-on experience in both frontend and backend development, I specialize in creating robust applications using technologies like Golang, Gin, Docker, Java, React, AWS Lambda etc.
           </p>
         </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInLeft" delay={0.2 * 1000} style={{ marginTop: "2rem", marginBottom: "2rem" }}>
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.1 * 500} style={{ marginTop: "2rem", marginBottom: "2rem" }}>
           <p>
           </p>
         </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInLeft" delay={0.3 * 1000}>
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.1 * 500}>
           <p>
           I'm passionate about creating efficient, scalable applications and have experience building APIs, optimizing backend systems, and designing user-friendly interfaces.</p>
         </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInLeft" delay={0.2 * 1000} style={{ marginTop: "2rem", marginBottom: "2rem" }}>
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.2 * 500} style={{ marginTop: "2rem", marginBottom: "2rem" }}>
           <p>
           </p>
         </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInLeft" delay={0.3 * 1000}>
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.2 * 500}>
           <p>
           At present, I've developed my skills in Golang, working on real-time data processing, serverless API development, My work spans distributed systems, SQL queries, and seamless integration of frontend and backend.</p>
         </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInLeft" delay={0.4 * 1000}>
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.2 * 500}>
           <h3>Here are my main skills:</h3>
         </ScrollAnimation>
         <div className="hard-skills">
           <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 1000}>
+            <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 500}>
               <img src={Golang} alt="golang" />
             </ScrollAnimation>
           </div>
           <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 1000}>
+            <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 500}>
               <img src={Gin} alt="golang" />
 
             </ScrollAnimation>
           </div>
           <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 1000}>
+            <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 500}>
               <img src={Docker} alt="docker" />
             </ScrollAnimation>
           </div>
           <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.13 * 1000}>
+            <ScrollAnimation animateIn="fadeInUp" delay={0.13 * 500}>
               <img src={reactIcon} alt="React" />
             </ScrollAnimation>
           </div>
           <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 1000}>
+            <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 500}>
               <img src={Java} alt="java" />
             </ScrollAnimation>
           </div>
           <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 1000}>
+            <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 500}>
               <img src={Postman} alt="postman" />
             </ScrollAnimation>
           </div>
           <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.16 * 1000}>
+            <ScrollAnimation animateIn="fadeInUp" delay={0.16 * 500}>
               <img src={nodeIcon} alt="Node" />
             </ScrollAnimation>
           </div>
           <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.17 * 1000}>
+            <ScrollAnimation animateIn="fadeInUp" delay={0.17 * 500}>
               <img src={htmlIcon} alt="Html" />
             </ScrollAnimation>
           </div>
           <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.18 * 1000}>
+            <ScrollAnimation animateIn="fadeInUp" delay={0.18 * 500}>
               <img src={cssIcon} alt="Css" />
             </ScrollAnimation>
           </div>
           <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 1000}>
+            <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 500}>
               <img src={jsIcon} alt="JavaScript" />
             </ScrollAnimation>
           </div>
